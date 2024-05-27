f934a14bbfc087de54c4eb5c78f61ef94f8c06bd
AliceOrlandini <a.orlandini10@studenti.unipi.it>
2024-05-27 16:08:18 +0000

    third version of the algorithm with 1 job and without the Counter

--- cloud-project/src/main/java/it/unipi/hadoop/LetterFrequencyReducer.java
+++ cloud-project/src/main/java/it/unipi/hadoop/LetterFrequencyReducer.java
@@ -9,17 +9,12 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class LetterFrequencyReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
 
-    public enum Counters {
-        TOTAL_LETTERS
-    }
-
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         long sum = 0;
         for (IntWritable val : values) {
             sum += val.get();
         }
-        context.getCounter(Counters.TOTAL_LETTERS).increment(sum);
         context.write(new Text(key), new LongWritable(sum));
     }
 }
