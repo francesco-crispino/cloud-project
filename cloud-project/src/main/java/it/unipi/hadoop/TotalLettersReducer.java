70cc5cef566a7d9b924675cce1f7d8ddc98cdb4e
AliceOrlandini <a.orlandini10@studenti.unipi.it>
2024-05-27 15:51:54 +0000

    bugfix

--- cloud-project/src/main/java/it/unipi/hadoop/TotalLettersReducer.java
+++ cloud-project/src/main/java/it/unipi/hadoop/TotalLettersReducer.java
@@ -17,7 +17,7 @@ public class TotalLettersReducer extends Reducer<Text, IntWritable, Text, LongWr
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         long sum = 0;
         for (IntWritable val : values) {
-            sum++;
+            sum += val.get();
         }
         context.getCounter(Counters.TOTAL_LETTERS).increment(sum);
         context.write(new Text(key), new LongWritable(sum));
