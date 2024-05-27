915ed6cc0543558c3b3a99508b4b9e5809aa22e3
AliceOrlandini <a.orlandini10@studenti.unipi.it>
2024-05-27 08:02:38 +0000

    commit pre changes

--- cloud-project/src/main/java/it/unipi/hadoop/LetterFrequencyReducer.java
+++ cloud-project/src/main/java/it/unipi/hadoop/LetterFrequencyReducer.java
@@ -3,6 +3,7 @@ package it.unipi.hadoop;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,8 +11,9 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class LetterFrequencyReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
 
-    private long totalLetters = 0; // Contatore totale delle lettere
+    // private long totalLetters = 0; // Contatore totale delle lettere
     private Map<Text, IntWritable> letterSums = new HashMap<>();
+    private static Map<Text, AtomicLong> sharedCounts = new HashMap<>();
 
     /*@Override
     protected void setup(Context context) throws IOException, InterruptedException {
@@ -20,20 +22,27 @@ public class LetterFrequencyReducer extends Reducer<Text, IntWritable, Text, Dou
 
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
+        AtomicLong count = sharedCounts.computeIfAbsent(key, k -> new AtomicLong(0));
+        for(IntWritable val: values) {
+            count.addAndGet(val.get());
         }
-        letterSums.put(new Text(key), new IntWritable(sum));
-        totalLetters += sum;
+        letterSums.put(new Text(key), new IntWritable((int)count.get()));
     }
 
     @Override
     public void cleanup(Context context) throws IOException, InterruptedException {
-        for(Text letter: letterSums.keySet()) {
+        /*for(Text letter: letterSums.keySet()) {
             int sum = letterSums.get(letter).get();
             double frequency = (double) sum / totalLetters;
             context.write(letter, new DoubleWritable(frequency));
+        }*/
+
+        long totalLetters = sharedCounts.values().stream().mapToLong(AtomicLong::get).sum();
+        for(Text letter: letterSums.keySet()) {
+            double sum = (double) letterSums.get(letter).get();
+            double frequency = sum / totalLetters;
+            context.write(letter, new DoubleWritable(frequency));
+            //context.write(letter, new DoubleWritable(sum));
         }
     }
 }
