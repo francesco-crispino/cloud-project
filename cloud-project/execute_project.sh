53f8ca8b0b1b22e3e1ef78a2e1cc92d042037e3a
tonioskr <a.nigro8@studenti.unipi.it>
2024-05-30 14:31:23 +0000

    adjustement on the execute_project.sh

--- cloud-project/execute_project.sh
+++ cloud-project/execute_project.sh
@@ -2,16 +2,16 @@
 
 # Verifica che siano stati forniti due argomenti
 if [ "$#" -ne 3 ]; then
-    echo "Please, inserisci nell'ordine: NOME_DATASET NOME_CARTELLA_OUTPUT"
+    echo "Please, inserisci nell'ordine: NOME_DATASET NOME_CARTELLA_OUTPUT_LETTER NOME_CARTELLA_OUTPUT"
     exit 1
 fi
 
 NOME_DATASET=$1
-NOME_CARTELLA_OUTPUT_FIRST=$2
+NOME_CARTELLA_OUTPUT_LETTER=$2
 NOME_CARTELLA_OUTPUT=$3
 
 # Esegui il comando hadoop jar
-hadoop jar ./target/cloud-project-1.0-SNAPSHOT.jar it.unipi.hadoop.LetterFrequency "$NOME_DATASET" "$NOME_CARTELLA_OUTPUT_FIRST" "$NOME_CARTELLA_OUTPUT"
+hadoop jar ./target/cloud-project-1.0-SNAPSHOT.jar it.unipi.hadoop.LetterFrequency "$NOME_DATASET" "$NOME_CARTELLA_OUTPUT_LETTER" "$NOME_CARTELLA_OUTPUT"
 
 # Verifica se il comando è riuscito
 if [ "$?" -eq 0 ]; then
