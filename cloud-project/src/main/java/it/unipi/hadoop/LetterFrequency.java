30fbd2140bd3204f34faace7e6d5021009807fd2
tonioskr <a.nigro8@studenti.unipi.it>
2024-06-04 16:48:12 +0000

    code adjustement

--- cloud-project/src/main/java/it/unipi/hadoop/LetterFrequency.java
+++ cloud-project/src/main/java/it/unipi/hadoop/LetterFrequency.java
@@ -1,4 +1,5 @@
 package it.unipi.hadoop;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -9,7 +10,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -19,6 +19,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -26,110 +27,96 @@ import org.apache.hadoop.fs.FileSystem;
 public class LetterFrequency {
 
     public static void main(String[] args) throws Exception {
-        final Integer MAX_NUM_OF_REDUCER = 5;
+        final Integer MAX_NUM_OF_REDUCER = 1;
 
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(conf);
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length < 2) {
-            System.err.println("Usage: letterfrequency <in> [<in>...] <out>");
+        if (otherArgs.length < 3) {
+            System.err.println("Usage: letterfrequency <in> <out_first_job> <out_second_job>");
             System.exit(2);
         }
 
-        List<String> list_document = new ArrayList<>();
-        list_document.add("ita-10_KB");
-        list_document.add("ita-100_KB");
-        list_document.add("ita-5_MB");
-        list_document.add("ita-400_MB");
-        list_document.add("ita-1_GB");
-        list_document.add("ita-3_GB");
-
         String input_file = otherArgs[0];
-        String output_dir_first_job_from_args = otherArgs[1];
-        String output_dir_second_job_from_args = otherArgs[2];
+        String output_dir_first_job = otherArgs[1];
+        String output_dir_second_job = otherArgs[2];
 
-        
-        for(String file_name : list_document){
+        BufferedWriter executionTimesWriter;
+        Path execution_times_path = new Path(output_dir_first_job + "/execution_times.txt");
+        FSDataOutputStream execution_times_out = fs.create(execution_times_path, true);
+        executionTimesWriter = new BufferedWriter(new OutputStreamWriter(execution_times_out, StandardCharsets.UTF_8));
+        executionTimesWriter.write("reducers_nums,execution_time");
+        executionTimesWriter.newLine();
 
-            input_file = "file_ita/"+file_name+".txt";
-            String output_dir_first_job = output_dir_first_job_from_args + ("_" + file_name);
-            String output_dir_second_job = output_dir_second_job_from_args + ("_" + file_name);
+         for (int reducer_nums = 1; reducer_nums <= MAX_NUM_OF_REDUCER; reducer_nums += 3) {
 
-            BufferedWriter executionTimesWriter = null;
-            Path execution_times_path = new Path(output_dir_first_job + "/execution_times.txt");
-            FSDataOutputStream execution_times_out = fs.create(execution_times_path, true);
-            executionTimesWriter = new BufferedWriter(new OutputStreamWriter(execution_times_out, StandardCharsets.UTF_8));
-            executionTimesWriter.write("reducers_nums,execution_time");
-            executionTimesWriter.newLine();
+            // -------- START HADOOP ALGORITHM ---------
 
-            for (int reducer_nums = 1; reducer_nums <= MAX_NUM_OF_REDUCER; reducer_nums += 1) {
+            Job job1 = Job.getInstance(conf, "letter count");
+            job1.setJarByClass(LetterFrequency.class);
+            job1.setMapperClass(LetterCountMapper.class);
+            //since we have the in-mapper combiner we don't have to use the combiner
+            job1.setReducerClass(LetterCountReducer.class);
+            job1.setOutputKeyClass(Text.class);
+            job1.setOutputValueClass(LongWritable.class);
 
-                // ------ START HADOOP ALGORITHM ------
-                System.out.println("Analyzing the "+file_name+" file with num_reducer = "+reducer_nums);
-                Job job1 = Job.getInstance(conf, "letter count");
-                job1.setJarByClass(LetterFrequency.class);
-                job1.setMapperClass(LetterCountMapper.class);
-                //since we have the in-mapper combiner we don't have to use the combiner
-                job1.setReducerClass(LetterCountReducer.class);
-                job1.setOutputKeyClass(Text.class);
-                job1.setOutputValueClass(LongWritable.class);
+            FileInputFormat.addInputPath(job1, new Path(input_file));
 
-                FileInputFormat.addInputPath(job1, new Path(input_file));
+            Path resultsDir = new Path(output_dir_first_job, "total_letters_count" + reducer_nums);
+            FileOutputFormat.setOutputPath(job1, resultsDir);
 
-                Path resultsDir = new Path(output_dir_first_job, "total_letters_count" + reducer_nums);
-                FileOutputFormat.setOutputPath(job1, resultsDir);
+            long startTime = System.currentTimeMillis();
+            job1.waitForCompletion(true);
 
-                long startTime = System.currentTimeMillis();
-                job1.waitForCompletion(true);
+            // open and read the output of the previous file
+            Path outputFile = new Path(resultsDir, "part-r-00000");
+            FSDataInputStream inputStream = fs.open(outputFile);
 
-                // open and read the output of the previous file
-                Path outputFile = new Path(resultsDir, "part-r-00000");
-                FSDataInputStream inputStream = fs.open(outputFile);
-                // open and read the output of the previous file
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                String number = " ";
-                if ((line = reader.readLine()) != null) {
-                    Pattern pattern = Pattern.compile("\\d+");
-                    Matcher matcher = pattern.matcher(line);
-                    if (matcher.find()) {
-                        number = matcher.group();   
-                    }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            String number = " ";
+            if ((line = reader.readLine()) != null) {
+                Pattern pattern = Pattern.compile("\\d+");
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    number = matcher.group();   
                 }
-                else{
-                    System.err.println("No value printed from the job");
-                    System.exit(0);
-                }
-                
-                System.out.println("The total letter count is: " + number);
-
-                //lunch the second job that will compute the letter frequency
-                conf.set("letterCount",number);
-                Job job2 = Job.getInstance(conf, "letter frequency");
-                job2.setJarByClass(LetterFrequency.class);
-                job2.setMapperClass(LetterFrequencyMapper.class);
-                job2.setReducerClass(LetterFrequencyReducer.class);
-                job2.setOutputKeyClass(Text.class);
-                job2.setOutputValueClass(LongWritable.class);
-                job2.setNumReduceTasks(reducer_nums);
-                
-                FileInputFormat.addInputPath(job2, new Path(input_file));
-                
-                FileOutputFormat.setOutputPath(job2, new Path(output_dir_second_job));
-
-                resultsDir = new Path(output_dir_second_job, "letter_frequency" + reducer_nums);
-                FileOutputFormat.setOutputPath(job2, resultsDir);
-
-                job2.waitForCompletion(true);
-                // ------ END HADOOP ALGORITHM ------
-
-                long endTime = System.currentTimeMillis();
-                System.out.println("Job with " + reducer_nums + " reducers took " + (endTime - startTime) + " milliseconds");
-                executionTimesWriter.write(reducer_nums + "," + (endTime - startTime));
-                executionTimesWriter.newLine();
             }
-            executionTimesWriter.close();
+            else{
+                System.err.println("No value printed from the job");
+                System.exit(0);
+            }
+
+            System.out.println("The total letter count is: " + number);
+
+             //lunch the second job that will compute the letter frequency
+            conf.set("letterCount",number);
+            Job job2 = Job.getInstance(conf, "letter frequency");
+            job2.setJarByClass(LetterFrequency.class);
+            job2.setMapperClass(LetterFrequencyMapper.class);
+            //since we have the in-mapper combiner we don't have to use the combiner
+            job2.setReducerClass(LetterFrequencyReducer.class);
+            job2.setOutputKeyClass(Text.class);
+            job2.setOutputValueClass(LongWritable.class);
+            job2.setNumReduceTasks(reducer_nums);
+
+
+            FileInputFormat.addInputPath(job2, new Path(input_file));
+            
+            FileOutputFormat.setOutputPath(job2, new Path(output_dir_second_job));
+
+            resultsDir = new Path(output_dir_second_job, "letter_frequency" + reducer_nums);
+            FileOutputFormat.setOutputPath(job2, resultsDir);
+
+             job2.waitForCompletion(true);
+            // -------- END HADOOP ALGORITHM ---------
+
+             long endTime = System.currentTimeMillis();
+            System.out.println("Job with " + reducer_nums + " reducers took " + (endTime - startTime) + " milliseconds");
+            executionTimesWriter.write(reducer_nums + "," + (endTime - startTime));
+            executionTimesWriter.newLine();
+            if(reducer_nums == 25) reducer_nums = 23; // this one to have 26 reducer as the number of letters
         }
+        executionTimesWriter.close();
     }
 }
